e1bfb4329f05d0ebe700d3f432a88d30e255f55f
chchaaa12 <114151989+merysahhh@users.noreply.github.com>
2022-12-12 19:04:20 +0700

    Praktikum6
    Selesai

---
+++ Praktikum6/tugas6.php
@@ -0,0 +1,51 @@
+<?php
+function angkaRomawi($angka)
+{
+	 $angka = intval($angka);
+	 $result = '';
+	 
+	 $array = array('M' => 1000,
+	 'CM' => 900,
+	 'D' => 500,
+	 'CD' => 400,
+	 'C' => 100,
+	 'XC' => 90,
+	 'L' => 50,
+	 'XL' => 40,
+	 'X' => 10,
+	 'IX' => 9,
+	 'V' => 5,
+	 'IV' => 4,
+	 'I' => 1);
+	 
+	 foreach($array as $roman => $value){
+	  $matches = intval($angka/$value);
+	 
+	  $result .= str_repeat($roman,$matches);
+	 
+	  $angka = $angka % $value;
+	 }
+	 
+	 return $result;
+}
+
+?>
+<html>
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <title>KONVERSI ROMAWI - Merysah</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+
+		<form action="#" method="POST" align="center">
+		Angka : <input type="number" name="angka"><br><br>
+		<button type="submit">Konversi</button>
+		<br>Romawi : <?php error_reporting(0); echo angkaRomawi($_POST["angka"]); ?><br>
+		<br><br><p class="foot">merysah-180</p>
+		</form>
+	</body>
+</html>
+
